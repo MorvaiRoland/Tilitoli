c8099d5db737a25004de5096c3a520a3dac331d9
MorvaiRoland <71487395+MorvaiRoland@users.noreply.github.com>
2020-12-17 19:32:34 +0100

    TiliToli #3
    keverés működik

--- TiliToli/TiliToli/MainWindow.xaml.cs
+++ TiliToli/TiliToli/MainWindow.xaml.cs
@@ -45,14 +45,46 @@ namespace TiliToli
 
                 allas[nullaGombIndex] = allas[ezGombFelirat];
                 allas[ezGombFelirat] = 0;
+              
 
-           
                 if (allas.SequenceEqual(kesz))
                 {
-                    
+                    MessageBox.Show("Ügyes vagy! ");
                 }
                 
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button0.Visibility = Visibility.Hidden;
+            
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                Button ezGomb = (Button)FindName("Button" + Convert.ToString(r.Next(1, 9)));
+                Button nullaGomb = (Button)FindName("Button0");
+
+                var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
+                var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
+
+                int ezGombFelirat = int.Parse(ezGomb.Content.ToString());
+                int ezGombIndex = Array.IndexOf(allas, ezGombFelirat);
+                int nullaGombIndex = Array.IndexOf(allas, 0);
+
+                if ((fTav == 100 && vTav == 0) || (vTav == 100 && fTav == 0))
+                {
+                    var seged = ezGomb.Margin;
+                    ezGomb.Margin = nullaGomb.Margin;
+                    nullaGomb.Margin = seged;
+
+                    var seged2 = ezGombFelirat;
+                    allas[nullaGombIndex] = seged2;
+                    allas[ezGombIndex] = 0;
+                }
+            }
+        }
     }
 }
+    
+
